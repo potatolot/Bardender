cec7706f71a8cd90835591235abe9b5a0ed33620
potatolot <ln.dejonge@outlook.com>
2023-10-04 09:58:15 +0200

    Initial Upload
    First upload of the Unreal Project

---
+++ Source/Bardender/CPDialogSystem.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPDialogSystem.h"
+
+CPDialogSystem::CPDialogSystem()
+{
+}
+
+CPDialogSystem::~CPDialogSystem()
+{
+}
